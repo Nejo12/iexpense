c9935cf297f1885664ee79c3f8d971940aae3f2c
Gabriel <olaniyi_g@hotmail.com>
2019-04-26 03:37:57 +0200

    components test complete

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,23 +13,112 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
+database.ref().set({
+  name: "Gabriel Olaniyi",
+  age: 33,
+  stressLevel: 6,
+  job: {
+    title: "software developer",
+    company: "Google"
+  },
+  isSingle: false,
+  location: {
+    city: "Berlin",
+    Country: "Germany"
+  }
+});
+
+// database
+//   .ref("location/city")
+//   .set("Berlin")
+//   .then(() => {
+//     console.log("data changed successfully");
+//   })
+//   .catch(e => {
+//     console.log("Error changing data:", e);
+//   });
+
+database.ref("age").set(35);
+// database.ref("location/city").set("Poznan");
+// database.ref("location/Country").set("Poland");
+
+database.ref().update({
+  stressLevel: 9,
+  "job/company": "Amazon",
+  "location/city": "Seattle"
+});
+
+database
+  .ref("attributes")
+  .set({
+    height: "187cm",
+    weight: "90kg"
+  })
+  .then(() => {
+    // console.log("This is the resolved attribute msg!");
+  })
+  .catch(e => {
+    console.log("This is errer: ", e);
+  });
+
+// database
+//   .ref("isSingle")
+//   .remove()
+//   .then(() => {
+//     console.log("Data was removed");
+//   })
+//   .catch(e => {
+//     console.log("Removal was unsuccessful");
+//   });
+
+database.ref("expenses").push({
+  description: "WBread",
+  note: "Wheat bread for sandwish",
+  amount: "€3,00",
+  createdAt: "12:00"
+});
+
+database.ref("expenses").push({
+  description: "sweet bread",
+  note: "fortea",
+  amount: "€2,50",
+  createdAt: "12:30"
+});
+
+database.ref("expenses").push({
+  description: "bill",
+  note: "So much to pay",
+  amount: "€35,50",
+  createdAt: "13:00"
+});
+
+database.ref("expenses").on("value", snapshot => {
+  const val = snapshot.val();
+  // console.log(val);
+});
+
+database.ref().on("value", snapshot => {
+  const val = snapshot.val();
+  console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+});
+
 database.ref("expenses").on("child_removed", snapshot => {
-  console.log(snapshot.key, snapshot.val());
+  console.log("child removed: ", snapshot.key, snapshot.val());
 });
 
 database.ref("expenses").on("child_changed", snapshot => {
-  console.log(snapshot.key, snapshot.val());
+  console.log("child changed: ", snapshot.key, snapshot.val());
 });
 
 database.ref("expenses").on("child_added", snapshot => {
-  console.log(snapshot.key, snapshot.val());
+  console.log("child added: ", snapshot.key, snapshot.val());
 });
+
 // database
 //   .ref("expenses")
 //   .once("value")
 //   .then(snapshot => {
 //     const expenses = [];
-
 //     snapshot.forEach(childSnapshot => {
 //       expenses.push({
 //         id: childSnapshot.key,
@@ -39,26 +128,17 @@ database.ref("expenses").on("child_added", snapshot => {
 //     console.log(expenses);
 //   });
 
-// database.ref("expenses").on("value", snapshot => {
-//   const val = snapshot.val();
-//   console.log(val);
-// });
-// database.ref("expenses").push({
-//   description: "Bought condoms",
-//   note: "Actor never dies",
-//   amount: "€10,50",
-//   createdAt: "11:30"
-// });
-
-// database.ref().on("value", snapshot => {
-//   const val = snapshot.val();
-//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
-// });
-
-// database.ref().on(
+// database.ref("expenses").on(
 //   "value",
 //   snapshot => {
-//     console.log(snapshot.val());
+//     const expenses = [];
+//     snapshot.forEach(childSnapshot => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+//     console.log(expenses);
 //   },
 //   e => {
 //     console.log("Error fetching data : ", e);
@@ -70,7 +150,7 @@ database.ref("expenses").on("child_added", snapshot => {
 // }, 3500);
 
 // setTimeout(() => {
-//   database.ref().off("value");
+//   database.ref("age").off("value");
 // }, 7000);
 
 // setTimeout(() => {
@@ -86,63 +166,4 @@ database.ref("expenses").on("child_added", snapshot => {
 //   })
 //   .catch(e => {
 //     console.log("Error fetching data : ", e);
-//   });
-// database.ref().set({
-//   name: "Gabriel Olaniyi",
-//   age: 33,
-//   stressLevel: 6,
-//   job: {
-//     title: "software developer",
-//     company: "Google"
-//   },
-//   isSingle: false,
-//   location: {
-//     city: "Berlin",
-//     Country: "Germany"
-//   }
-// });
-
-// database
-//   .ref("location/city")
-//   .set("Berlin")
-//   .then(() => {
-//     console.log("data changed successfully");
-//   })
-//   .catch(e => {
-//     console.log("Error changing data:", e);
-//   });
-
-// database.ref().set("This is my Data.");
-
-// database.ref("age").set(35);
-// database.ref("location/city").set("Poznan");
-// database.ref("location/Country").set("Poland");
-
-// database.ref().update({
-//   stressLevel: 9,
-//   "job/company": "Amazon",
-//   "location/city": "Seattle"
-// });
-
-// database
-//   .ref("attributes")
-//   .set({
-//     height: "187cm",
-//     weight: "90kg"
-//   })
-//   .then(() => {
-//     console.log("This is the resolved msg!");
-//   })
-//   .catch(e => {
-//     console.log("This is errer: ", e);
-//   });
-
-// database
-//   .ref("isSingle")
-//   .remove()
-//   .then(() => {
-//     console.log("Data was removed");
-//   })
-//   .catch(e => {
-//     console.log("Removal was unsuccessful");
 //   });
--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import { ExpenseListFilters } from "../../components/ExpenseListFilters";
 import { filters, altFilters } from "../fixtures/filters";
+import moment from "moment";
 
 let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
 beforeEach(() => {
@@ -31,4 +32,40 @@ test("Should render expenselistfilters with alt data correctly", () => {
     filters: altFilters
   });
   expect(wrapper).toMatchSnapshot();
+});
+
+test("Should handle text change", () => {
+  const value = "Insurance";
+  wrapper.find("input").simulate("change", {
+    target: { value }
+  });
+  expect(setTextFilter).toHaveBeenLastCalledWith(value);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("Should sort by date", () => {
+  const value = "date";
+  wrapper.setProps({ filters: altFilters });
+  wrapper.find("select").simulate("change", { target: { value } });
+  expect(sortByDate).toHaveBeenCalled();
+});
+
+test("Should sort by amount", () => {
+  const value = "amount";
+  wrapper.find("select").simulate("change", { target: { value } });
+  expect(sortByAmount).toHaveBeenCalled();
+});
+
+test("Should handle date change", () => {
+  const startDate = moment(0).add(4, "years");
+  const endDate = moment(0).add(8, "years");
+  wrapper.find("DateRangePicker").prop("onDatesChange")({ startDate, endDate });
+  expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+  expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+});
+
+test("Should handle date focus change", () => {
+  const calendarFocused = "endDate";
+  wrapper.find("DateRangePicker").prop("onFocusChange")(calendarFocused);
+  expect(wrapper.state("calendarFocused")).toBe(calendarFocused);
 });
