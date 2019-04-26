f12d50be6b2674bdf8798607cd4627fc9aeffb82
Gabriel <olaniyi_g@hotmail.com>
2019-04-26 13:30:06 +0200

    Resolving Github issue

--- src/app.js
+++ src/app.js
@@ -9,7 +9,6 @@ import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "./firebase/firebase";
-import "./samples/promises";
 import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
